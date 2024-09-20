4fe8126a7b5d51ebad0da4d03e9cec272293475c
Joshua Nanninga <jmarknanninga@gmail.com>
2024-09-20 10:51:33 -0500

    updated app.js

--- app.js
+++ app.js
@@ -1,24 +1,48 @@
-// src/App.js
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home';
-import Products from './pages/Products';
-import CartPage from './pages/Cart';
-import Checkout from './pages/Checkout';
-import ProductDetail from '../components/ProductDetail';
+
+const Home = lazy(() => import('./pages/Home'));
+const Products = lazy(() => import('./pages/Products'));
+const CartPage = lazy(() => import('./pages/Cart'));
+const Checkout = lazy(() => import('./pages/Checkout'));
+const ProductDetail = lazy(() => import('../components/ProductDetail'));
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children; 
+  }
+}
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/products" exact component={Products} />
-        <Route path="/products/:id" component={ProductDetail} />
-        <Route path="/cart" component={CartPage} />
-        <Route path="/checkout" component={Checkout} />
-      </Switch>
+      <ErrorBoundary>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/products" exact component={Products} />
+            <Route path="/products/:id" component={ProductDetail} />
+            <Route path="/cart" component={CartPage} />
+            <Route path="/checkout" component={Checkout} />
+          </Switch>
+        </Suspense>
+      </ErrorBoundary>
     </Router>
   );
 }
 
 export default App;
+
